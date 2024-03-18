47e96e79b3e34bcad1d4132858d502730f066d84
shruti <shrutisachpara.shivinfotech@gmail.com>
2024-03-18 23:13:48 +0530

    Add project files.

---
+++ ConsumerRMQ/Program.cs
@@ -0,0 +1,21 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+Console.WriteLine("This is Admin Consumer");
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost",
+};
+var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+var consumer=new EventingBasicConsumer(channel);
+consumer.Received += (m, arge) =>
+{
+    var body = arge.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine(message);
+
+};
+channel.BasicConsume(queue: "AmzAdminQueue", autoAck: true, consumer: consumer);
+Console.WriteLine("consuming started");
+Console.ReadLine(); 
